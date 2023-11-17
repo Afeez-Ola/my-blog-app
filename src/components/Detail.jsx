d4e3d49d08f9efe0d2141ed4ba8e081a546f262f
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-11-17 23:57:32 +0100

    .

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 function Detail() {
  const { id } = useParams();
+
+ useEffect(() => {
+  fetch(`https://dummyjson.com/products/${id}`, {
+   method: 'GET',
+  })
+   .then((res) => console.log(res))
+   .catch((err) => console.log(err));
+ });
 
  return <p>Detail {id}</p>;
 }
