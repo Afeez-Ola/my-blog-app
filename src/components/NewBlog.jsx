937bc55336731ebb8becd5a4d3702cf5d5b0b10f
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-11-19 06:05:22 +0100

    new blog input fields

--- src/components/NewBlog.jsx
+++ src/components/NewBlog.jsx
@@ -1,46 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 function NewBlog() {
  return (
-  <div class='container'>
-   <div class='row'>
-    <div class='col-md-12'>
-     <form method='post' role='form'>
-      <div class='form-group'>
-       <input
-        type='text'
-        class='form-control'
-        name='title'
-        placeholder='Title'
-       />
-      </div>
-      <div class='form-group'>
-       <label> Image </label>
-       <div class='input-group'>
-        <span class='input-group-btn'>
-         <span class='btn btn-primary btn-file'>
-          Browse <input type='file' name='bimgs' multiple></input>
-         </span>
-        </span>
-        <input type='text' class='form-control' readonly></input>
-       </div>
-      </div>
-      <div class='form-group'>
-       <textarea class='form-control bcontent' name='content'></textarea>
-      </div>
-      <div class='form-group'>
-       <input
-        type='submit'
-        name='Submit'
-        value='Publish'
-        class='btn btn-primary form-control'
-       ></input>
-      </div>
-     </form>
-    </div>
+  <form className='new_form'>
+   <div>
+    <label htmlFor='title'>Title:</label>
+    <input type='text' id='title' required />
    </div>
-  </div>
+   <div>
+    <label htmlFor='content'>Content:</label>
+    <textarea id='content' required />
+   </div>
+   <div>
+    <label htmlFor='image'>Image URL:</label>
+    <input type='text' id='image' />
+   </div>
+   <button type='submit'>Submit</button>
+  </form>
  );
 }
 
