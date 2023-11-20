059f1d39aa83e50df37dc06a88ba2c5e799b40c5
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-11-20 01:57:11 +0100

    .

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -14,8 +14,10 @@ export function Blogs() {
  const description = faker.lorem.paragraph();
  const thumbnail = faker.image.image();
 
- const numImages = Math.floor(Math.random() * 4) + 1; // Random number of images (1 to 4)
- const images = Array.from({ length: numImages }, () => faker.image.imageUrl());
+ const numImages = Math.floor(Math.random() * 4) + 1; //
+ const images = Array.from({ length: numImages }, () =>
+  faker.image.urlLoremFlickr({ category: 'nature', width: 800, height: 600 }),
+ );
 
  return {
   id,
